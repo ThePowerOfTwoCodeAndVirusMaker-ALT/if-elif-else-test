ef8614e424f9f62b79ec238aa252629228b4dd75
Leo Cameron <153335418+ThePowerOfTwoCodeAndVirusMaker-ALT@users.noreply.github.com>
2023-12-10 05:46:33 -0800

    Add files via upload

---
+++ random python project.py
@@ -0,0 +1,11 @@
+#enter number here
+num = 0
+if num > 0:
+    print('Positive')
+#if num is greater than 0, prints 'Positive'.
+elif num == 0:
+    print('Zero')
+#if num is 0, prints 'Zero'.
+else:
+    print('Negative')
+#if num is less than 0, prints 'Negative'.
